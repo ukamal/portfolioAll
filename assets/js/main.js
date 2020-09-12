529e6cedafe16212f80bbf0a2c8a975c313be7f9
Md.Kamal Hossen <flkamal2016@gmail.com>
2020-09-12 13:48:48 +0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 $(window).on("load", function() {
 
-//     $(".loader .inner").fadeOut(1000, function() {
-//         $(".loader").fadeOut(750);
-//     });
+    $(".loader .inner").fadeOut(1000, function() {
+        $(".loader").fadeOut(750);
+    });
 
-// })
+})
 /*******************End of loader area***************/
 
 $(document).ready(function(){
